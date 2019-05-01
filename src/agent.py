ac2d8aee716d233c090371c3ed152935d0e25500
z5114131 <z5114131@ad.unsw.edu.au>
2019-05-01 17:58:23 +1000

    y

--- src/agent.py
+++ src/agent.py
@@ -27,7 +27,7 @@ boards = np.zeros((10, 10), dtype="int8")
 s = [".","X","O"]
 curr = 0 # this is the current board to play in
 
-depth_limit = 5 # Max depth iterate too
+depth_limit = 6 # Max depth iterate too
 
 
 # print a row
